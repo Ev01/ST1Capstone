4320cea543d6f2b856d24bcaf0c2ae25bc179659
Ev01 <evpevdev@optusnet.com.au>
2024-05-02 08:58:58 +1000

    Add predictor selection

--- analysis.py
+++ analysis.py
@@ -3,13 +3,18 @@ from scipy.stats import f_oneway
 from handle_data import *
 
 
-def print_correlation_analysis(df):
+def correlation_analysis(df):
+    """Return a list of the continous predictors that are correlated to the target variable."""
     correlation_data = df[["host_response_rate", "first_review", "last_review", "review_scores_rating", "log_price"]].corr()["log_price"]
-    print(correlation_data)
+    selected_predictors = correlation_data.loc[abs(correlation_data) > 0.5]
+    selected_predictors = list(selected_predictors.index)
+    selected_predictors.remove("log_price")
+    return selected_predictors
+
 
 
 def anova_test(df, target_variable, categorical_predictors):
-
+    """Return a list of categorical predictors that pass the anova test (and are related to the target variable)"""
     # Creating an empty list of final selected predictors
     selected_predictors = []
     #print('##### ANOVA Results ##### \n')
@@ -28,6 +33,10 @@ def anova_test(df, target_variable, categorical_predictors):
 
 if __name__ == "__main__":
     df = get_formatted_dataframe()
-    print_correlation_analysis(df)
+    selected_continous_predictors = correlation_analysis(df)
     categorical_predictors = ["beds", "bedrooms", "bathrooms", "accommodates", "cancellation_policy", "host_identity_verified", "instant_bookable", "cleaning_fee"]
-    anova_test(df, "log_price", categorical_predictors)
+    selected_categorical_predictors = anova_test(df, "log_price", categorical_predictors)
+
+    selected_predictors = selected_continous_predictors + selected_categorical_predictors
+
+    print("selected predictors:", selected_predictors)
